a7ebf6d24a77f1ec0833a57939a9604a19a18fcd
Kerfindo <brianestock123@gmail.com>
2017-02-21 00:04:39 -0500

    Home Layout

--- home.php
+++ home.php
@@ -27,28 +27,116 @@
 			</div>
 		</a>
 		<a href="news.php">
-			News
+			<div class="button navButton">
+				News
+			</div>
 		</a>
 		<a href="popularactivities.php">
-			Popular Activities
+			<div class="button navButton">
+				Popular Activities
+			</div>
 		</a>
 		<a href="churchyear.php">
-			Church Year
+			<div class="button navButton">
+				Church Year
+			</div>
 		</a>
 		<a href="youth.php">
-			Youth
+			<div class="button navButton">
+				Youth
+			</div>
 		</a>
 		<a href="outreach.php">
-			Outreach
+			<div class="button navButton">
+				Outreach
+			</div>
 		</a>
 		<a href="directions.php">
-			Directions
+			<div class="button navButton">
+				Directions
+			</div>
 		</a>
+	</div>
 	
-	
+	<div class="section">
+		<div id="newsFeed" class="small-6 columns">
+			<div class="center"><h4>News Feed</h4></div>
+			<h5>Highlighted News</h5>
+			<ul>
+				<li>
+					Title 1
+				</li>
+				<li>
+					Title 2
+				</li>
+			</ul>
+			<h5>Recent News</h5>
+			<ul>
+				<li>
+					Title 1
+				</li>
+				<li>
+					Title 2
+				</li>
+				<li>
+					Title 3
+				</li>
+			</ul>
+		</div>
+		<div id="weeklySchedule" class="small-6 columns">
+			<div class="center"><h4>Weekly Schedule</h4></div>
+			<ul>
+				<li>
+					Sunday Text
+				</li>
+				<li>
+					Monday Text
+				</li>
+				<li>
+					Tuesday Text
+				</li>
+				<li>
+					Wednesday Text
+				</li>
+				<li>
+					Thursday Text
+				</li>
+				<li>
+					Friday Text
+				</li>
+				<li>
+					Saturday Text
+				</li>
+			</ul>
+		<a href="weeklybulletin.php">
+			<div class="center">[View Weekly Bulletin]</div>
+		</a>
+		</div>
+	</div>
+	<div class="site-container">
+		<div class="center">
+			<a href="https://www.google.com/maps/place/Emmanuel+Lutheran+Church+of+Eastmont/@40.441999,-79.8151877,17z/data=!3m1!4b1!4m5!3m4!1s0x8834ebe94cd95457:0x1ad5d653dd6ce35b!8m2!3d40.441999!4d-79.812999">
+				<img src="img/map.png" alt="Map" style="width:100%;height:auto;">
+			</a>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="floatleft">
+			Worship</br>
+			Contact</br>
+			Address
+		</div>
+		<div class="floatright">
+			<a href="https://www.facebook.com/emmanueleastmont/">
+				<img src="img/facebook.jpeg" alt="Facebook" style="width:50%;height:50%;"></br>
+			</a>
+			Copyright © 2017
+		</div>
+	</div>
 	<script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
 	<script src="js/app.js"></script>
+	
 </body>
 </html>
