76aefb9758b5d77fdb02b10f57d15e43f97f3121
cme34 <cme34@pitt.edu>
2017-03-08 03:01:32 -0500

    Created Editing

---
+++ editpost.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+//If the user is not signed in, prevent them from accessing this page
+if (!isset($_SESSION["username"])) {
+	header("Location: home.php");
+}
+
+//If the user is not an admin, prevent them from accessing this page
+if (!($_SESSION["admin"] == 1 || $_SESSION["admin"] == 2)) {
+	header("Location: home.php");
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+	<title>Edit Post</title>
+	<meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/foundation.css" />
+	<link rel='stylesheet' media='screen and (max-width: 800px)' href='css/mobile.css' />
+	<link rel='stylesheet' media='screen and (min-width: 801px)' href='css/app.css' />
+	<?php include 'php scripts/navigator.php';?>
+	<?php include 'php scripts/footer.php';?>
+</head>
+<body>
+	<div id="wrapper">
+		<div class="content">
+			<h1>Edit Post</h1>
+			<div class="containerGroup">
+				<div class="container">
+					<?php
+					$loc = $_GET["loc"];
+					$id = $_GET["id"];
+					
+					//Connect to database
+					$db = new mysqli('localhost', 'root', '', 'emmanuel');
+					if ($db->connect_error) {
+						echo "<p class='error-text'>Connection with database failed. Please try again later.</p>";
+						die();
+					}
+					
+					//Check if credentials are valid
+					$dbloc = str_replace(' ', '', $loc);
+					$query = "SELECT * FROM posts$dbloc WHERE posts$dbloc.postid = '$id'";
+					$result = $db->query($query);
+					
+					if ($result->num_rows == 1) {
+						$row = $result->fetch_assoc();
+						$title = $row["title"];
+						$image = $row["image"];
+						$text = $row["text"];
+						$highlight = 0;
+						echo "<form Action='php scripts/editpostscript.php?loc=$loc&id=$id' Method='POST'>";
+						echo "	Title: <input class='textFeild' id='title' name='title' type='text'></input></br>";
+						echo "	Image Link: <input class='textFeild' id='image' name='image' type='text'></input></br>";
+						echo "	Text: <textarea class='textFeild' id='text' name='text' type='text' rows=12></textarea></br>";
+						if ($loc == "news") {
+							$highlight = $row["highlight"];
+							echo "<input type='checkbox' id='highlight' name='highlight'><span>Highlight Post</span><br/>";
+						}
+						echo "	<button class='medium success button' id='buttonLogin'>Submit</button>";
+						echo "	<a href='home.php'><div class='medium secondary button' id='buttonCreateaccountCancel'>Cancel</div></a>";
+						if (isset($_SESSION["error"])) {
+							$err = $_SESSION["error"];
+							unset($_SESSION["error"]);
+							echo "<p class='error-text'>$err</br></p>";
+						}
+					}
+					else {
+						echo "<p>An error has occured. Please try again later.</p>";
+						echo "<a href='home.php'><div class='medium success button'>Back to home page</div></a>";
+					}
+					?>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+		<?php
+		createFooter();    //Create the footer at the bottom of the page. This is defined in footer.php
+		createNavigator(); //Create the navigator at the top of the page. This is defined in navigator.php
+		?>
+	</div>
+	
+	<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+	<script src="js/app.js"></script>
+	<script type="text/javascript">
+		(function()	{
+			$(document).on('ready', function() {
+				var loc = <?php echo json_encode($loc); ?>;
+				var title = <?php echo json_encode($title); ?>;
+				var image = <?php echo json_encode($image); ?>;
+				var text = <?php echo json_encode($text); ?>;
+				if (loc == "news") {
+					var highlight = <?php echo $highlight; ?>;
+					if (highlight == 1) {
+						$('#highlight').prop('checked', true);
+					}
+				}
+				$('#title').val(title);
+				$('#image').val(image);
+				$('#text').val(text);
+			});
+		})();
+	</script>
+</body>
+</html>
